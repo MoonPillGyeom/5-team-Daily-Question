5f537270cdaa16caf7f56c80d10c6eaf1a4e98a0
BBoBBoM <tkrhkakt12@gmail.com>
2023-11-08 10:56:30 +0900

    2023-11-08 재환 문제수정

---
+++ 20231108Question/index.js
@@ -0,0 +1,33 @@
+//1번문제 답안작성 
+const date = document.querySelector('#title');
+
+
+
+
+//2번문제 답안작성
+
+const fruit = document.querySelector("#fruit");
+const fruitList = fruit.children;
+//필요 요소 생성 내용 추가에 필요한 내용 작성해주세요
+
+function textDeco(e) {
+//함수를 작성해주세요
+
+
+//setTimeout은 2초가 지나면 클래스를 삭제하여 css효과도 같이 사라지게 구현 기본제공
+  setTimeout(function () {
+    e.target.classList.remove("line", "color", "pointer");
+  }, 2000);
+}
+
+
+
+//3번문제 답안
+//특정 요소 삭제에 관한 질문입니다.
+
+
+
+
+//4번문제 답안
+const todolist = document.getElementById('to-do-list');
+//내용 변경 css font 효과 속성 부여 추가 등의 일련의 과정을 작성해주세요
